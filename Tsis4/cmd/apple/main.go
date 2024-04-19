fcac41407cc022619c89a3b3acede096a347025b
Yuriy Kim <86676168+kim0111@users.noreply.github.com>
2024-04-19 20:58:31 +0500

    Tsis4 (AUTHORIZATION, AUTHENTICATION, PERMISSION)
    Also add store handler

---
+++ Tsis4/cmd/apple/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"github.com/kim0111/GoMidterm/pkg/vcs"
+	"os"
+	"sync"
+
+	"github.com/kim0111/GoMidterm/pkg/apple/model"
+	"github.com/kim0111/GoMidterm/pkg/jsonlog"
+
+	_ "github.com/lib/pq"
+)
+
+// Set version of application corresponding to value of vcs.Version.
+var (
+	version = vcs.Version()
+)
+
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn string
+	}
+}
+
+type application struct {
+	config config
+	models model.Models
+	logger *jsonlog.Logger
+	wg     sync.WaitGroup
+}
+
+func main() {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 8081, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://postgres:8956@localhost:5432/finalgo?sslmode=disable", "PostgreSQL DSN")
+	flag.Parse()
+
+	logger := jsonlog.NewLogger(os.Stdout, jsonlog.LevelInfo)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.PrintError(err, nil)
+		return
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			logger.PrintFatal(err, nil)
+		}
+	}()
+
+	app := &application{
+		config: cfg,
+		models: model.NewModels(db),
+		logger: logger,
+	}
+
+	if err := app.serve(); err != nil {
+		logger.PrintFatal(err, nil)
+	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
